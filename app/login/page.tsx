a55169e782f9658631cf555005e3c65588dbacb3
Dimas Alamsyah <dimas.alamsyah@bsa-logistics.com>
2024-02-15 15:23:59 +0700

    dev

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,7 +24,7 @@ export default function Login() {
       console.log(session);
       router.push("/dashboard");
     }
-  }, []);
+  }, [session]);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
